7bc48f19873a769c9d52952010b1382408de394a
Matteo Greco <matteo@hlpy.co>
2022-10-28 22:58:27 +0200

    firts sketch for navbar

--- components/Builder.tsx
+++ components/Builder.tsx
@@ -1,12 +1,20 @@
 function Builder() {
   return (
     <>
-      <bd-navbar></bd-navbar>
+      <bd-navbar
+        title="test"
+        color="black"
+        src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1eccef61-2e9d-4e1a-9481-5d7e21547651/d5mnvq2-394933b4-52a9-4f54-af2c-588ac80e4371.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFlY2NlZjYxLTJlOWQtNGUxYS05NDgxLTVkN2UyMTU0NzY1MVwvZDVtbnZxMi0zOTQ5MzNiNC01MmE5LTRmNTQtYWYyYy01ODhhYzgwZTQzNzEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.OnDHFffcQgmo1Re6V9jHBfnw81jd77mJL47tqq2ptdU"
+        buttons={[
+          {"name": "test1", "color": "white", "href": "test"},
+          {"name":"test2", "color": "white", "href": "test"}, 
+          {"name":"test2", "color": "white", "href": "test"}
+        ]}
+        height="100"
+        width="100"
+      ></bd-navbar>
 
-      <bd-navbar></bd-navbar>
-
-      <bd-navbar></bd-navbar>
-    </>
+      </>
   );
 }
 
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,13 +1,55 @@
 import { customElement } from "solid-element";
 import styles from "./Navbar.css"
+import { For } from 'solid-js';
 
-customElement("bd-navbar", { title: 'one', icon: 'two' }, (props: any, { element }) => {
-  return (
-    <>
-      <style>{styles}</style>
-      <div>
-        <p class={ 'pippo' }> ciao</p>
-      </div>
-    </>
-  );
-});
+interface NavbarProps{
+    title: string, 
+    color: string,
+    src: string, 
+    buttons: string,
+    height: string,
+    with: string
+}
+
+
+customElement("bd-navbar", 
+  { 
+    title: '', 
+    color: '',
+    src: '', 
+    buttons: '',
+    height: '',
+    with: ''
+  },
+  (props: NavbarProps, {}) => {
+    return (
+      <>
+        <style>{styles}</style>
+        <div>
+          <nav class="{ Nav }" style={'background-color: '+props.color}>
+            <div class="{ NavDivImg }">
+              <img 
+                class="{ NavImg }" 
+                width="100" 
+                height="100" 
+                src={props.src}></img>
+              <span class="{ NavTitle }">{props.title}</span>
+            </div>
+            <div class="{ NavDivMenu }">
+              <div class="{ NavDivMenuInner }">
+                <For each={props.buttons}>{(button:any) =>
+
+                  <a 
+                    href="{button.href}" 
+                    style={'color: '+button.color}
+                    class="{ MenuVoice }">
+                    {button.name}
+                  </a>
+                }</For>
+              </div>
+            </div>
+          </nav>
+        </div>
+        </>);
+  });
+
